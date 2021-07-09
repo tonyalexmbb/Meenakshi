adbf8bb1f491f2d7d4f95143092b3280d8c507f9
vishnu-kayamkulam <86889590+vishnu-kayamkulam@users.noreply.github.com>
2021-07-09 17:21:37 +0530

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -1,115 +1,82 @@
 const Asena = require('../events');
-const ffmpeg = require('fluent-ffmpeg');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
-const { MessageType } = require('@adiwajshing/baileys');
+const ffmpeg = require('fluent-ffmpeg');
+const {execFile} = require('child_process');
+const cwebp = require('cwebp-bin');
+const Config = require('../config');
+
 const Language = require('../language');
-const Lang = Language.getString('voicy');
-const conf = require('../config');
+const Lang = Language.getString('sticker');
 
+if (Config.WORKTYPE == 'private') {
+    Asena.addCommand({pattern: 'sticker$', fromMe: true, desc: Lang.STICKER_DESC}, (async (message, match) => {    
 
-const recognizeAudio = () => {
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
 
-    const headers = new Headers({
-        'Content-Type': 'audio/wav',
-        "Authorization": `Bearer ${conf.WITAI_API}`,
-        'Cache-Control': 'no-cache',
-        'Transfer-Encoding': 'chunked'
-    })
+        if (message.reply_message.video === false && message.reply_message.image) {
+            ffmpeg(location)
+                .outputOptions(["-y", "-vcodec libwebp"])
+                .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+                .save('st.webp')
+                .on('end', async () => {
+                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
+            });
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 
-    const requestBody = {
-        method: "POST",
-        body: fs.readFileSync('output.wav'),
-        headers: headers
-    }
-
-    return fetch("https://api.wit.ai/speech?v=20200219", requestBody)
-        .then(response => response.json())
-        .then(json => json._text)
-}
-
-const convertToWav = file => {
-    return ffmpeg(file)
-        .audioCodec('pcm_s16le')
-        .format('wav')
-        .save('output.wav')
-}
-
-
-if (conf.WORKTYPE == 'private') {
-
-    Asena.addCommand({ pattern: 'voicy', desc: Lang.USAGE, fromMe: true }, (async (message, match) => {
-
-        try {
-            if (message.reply_message) {
-                if (!message.reply_message.text && !message.reply_message.video && !message.reply_message.image) {
-                    const file = await message.client.downloadAndSaveMediaMessage({
-                        key: {
-                            remoteJid: message.reply_message.jid,
-                            id: message.reply_message.id
-                        },
-                        message: message.reply_message.data.quotedMessage
-                    })
-
-
-                    convertToWav(file).on('end', async () => {
-                        const recognizedText = await recognizeAudio()
-
-                        await message.client.sendMessage(message.jid, Lang.TEXT + '```' + recognizedText + '```', MessageType.text)
-                    });
-
-
-                } else {
-                    await message.client.sendMessage(message.jid, Lang.ONLY_AUDIO, MessageType.text)
-
-                }
-            } else {
-                await message.client.sendMessage(message.jid, Lang.NEED_REPLY, MessageType.text)
-
-            }
-
-        } catch (err) {
-            console.log(err)
         }
 
-
+        ffmpeg(location)
+            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 600x600"])
+            .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+            .save('sticker.webp')
+            .on('end', async () => {
+                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
+            });
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
 }
-if (conf.WORKTYPE == 'public') {
+else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'voicy', desc: Lang.USAGE, fromMe: false }, (async (message, match) => {
+    Asena.addCommand({pattern: 'sticker$', fromMe: false, desc: Lang.STICKER_DESC}, (async (message, match) => {    
 
-        try {
-            if (message.reply_message) {
-                if (!message.reply_message.text && !message.reply_message.video && !message.reply_message.image) {
-                    const file = await message.client.downloadAndSaveMediaMessage({
-                        key: {
-                            remoteJid: message.reply_message.jid,
-                            id: message.reply_message.id
-                        },
-                        message: message.reply_message.data.quotedMessage
-                    })
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
 
+        if (message.reply_message.video === false && message.reply_message.image) {
+            ffmpeg(location)
+                .outputOptions(["-y", "-vcodec libwebp"])
+                .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+                .save('st.webp')
+                .on('end', async () => {
+                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
+            });
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 
-                    convertToWav(file).on('end', async () => {
-                        const recognizedText = await recognizeAudio()
-
-                        await message.client.sendMessage(message.jid, Lang.TEXT + '```' + recognizedText + '```', MessageType.text)
-                    });
-
-
-                } else {
-                    await message.client.sendMessage(message.jid, Lang.ONLY_AUDIO, MessageType.text)
-
-                }
-            } else {
-                await message.client.sendMessage(message.jid, Lang.NEED_REPLY, MessageType.text)
-
-            }
-
-        } catch (err) {
-            console.log(err)
         }
 
-
+        ffmpeg(location)
+            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 600x600"])
+            .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+            .save('sticker.webp')
+            .on('end', async () => {
+                await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
+            });
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
 }
